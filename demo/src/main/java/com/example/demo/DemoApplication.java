c2ec31437577b5b59cdb1b5aade33de0208c2a47
loq <ALE@LAPTOP-UUM7DVB6>
2025-10-30 13:38:34 +0700

    initialcommit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	// Endpoint sederhana
+	@GetMapping("/")
+	public String home() {
+		return "Hello, ini website sederhana Spring Boot dengan Maven!";
+	}
+
+}
